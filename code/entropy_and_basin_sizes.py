15b11f822f2f58bd0484de74115cd0b3271d4245
Bryan Daniels <bryan.daniels.1@asu.edu>
2023-07-03 11:20:27 -0700

    add explicit min and max entropy functions

--- code/entropy_and_basin_sizes.py
+++ code/entropy_and_basin_sizes.py
@@ -6,15 +6,24 @@ def basin_entropy(w):
     # use masked array (ma) to avoid problems with zero
     return -np.dot(w,np.ma.log2(w).filled(0.))
 
+def min_basin_entropy(r,n):
+    return (r-1)*n/2**n -(1-(r-1)/2**n)*np.log2((1-(r-1)/2**n))
+
+def max_basin_entropy(r):
+    """
+    Note: This does not include a restriction due to having an
+    integer number of states in each basin.  This could produce
+    a bug in sampling near to the maximum entropy.
+    """
+    return np.log2(r)
+
 def entropy_to_relative_basin_sizes(h_tilde,r,n,tol=1e-3):
     
-    h_tilde_min = (r-1)*n/2**n -(1-(r-1)/2**n)*np.log2((1-(r-1)/2**n))
-    
-    if h_tilde > np.log2(r): 
+    if h_tilde > max_basin_entropy(r):
         raise ValueError("Basin entropy cannot be larger than log2(r)")
         
-    if h_tilde < h_tilde_min:
-        raise Exception("h_tilde below h_tilde_min = "+str(h_tilde_min))
+    if h_tilde < min_basin_entropy(r,n):
+        raise ValueError("Basin entropy cannot be below h_tilde_min = "+str(min_basin_entropy(r,n)))
         
     func = lambda w: (basin_entropy(w) - h_tilde)**2
     w0 = [ (i+1)/(r*(r+1)/2) for i in range(r) ]
@@ -26,11 +35,11 @@ def entropy_to_relative_basin_sizes(h_tilde,r,n,tol=1e-3):
     else:
         raise Exception("Solution not found within given tolerance")
 
-def entropy_to_basin_sizes(h_tilde,r,n):
-	"""
-	To do: Add checks to this!!!
-	"""
-    w = entropy_to_relative_basin_sizes(h_tilde,r,n)
+def entropy_to_basin_sizes(h_tilde,r,n,tol=1e-3):
+    """
+    To do: Add checks to this!!!
+    """
+    w = entropy_to_relative_basin_sizes(h_tilde,r,n,tol=tol)
     w_int = [round(w[_]*2**n) for _ in range(len(w))]
     w_int[-1] = w_int[-1] +  2**n - np.sum(w_int)
     return np.array(w_int)
